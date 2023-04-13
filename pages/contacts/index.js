2bcc37e86b0a9488c5fc89c6d5ed15caa35f894e
DanieleCastroBRRJ <danielecastro.brrj@gmail.com>
2023-04-12 21:47:18 -0300

    Continuando

--- pages/contacts/index.js
+++ pages/contacts/index.js
@@ -19,15 +19,22 @@ function sendContact(ev) {
 
     const formData = new FormData(ev.target);
 
-    for (const [key, value] of formData) {
+    formData.forEach((value, key) => {
         formJSON[key] = value
-    }
- 
-    formJSON = JSON.stringify(formJSON)
+    })
 
-    console.log(formJSON)
-    
-    // console.log(JSON.parse(formJSON))
+    $.post('http://localhost:3000/contacts', formJSON)
+    .done((data) => {
+        if(data.status == 'success') {
+            var firstName = formJSON.name.split(' ')[0]
+            var feedback = `
+                <h3>Olá ${firstName}!</h3>
+                <p>Seu contato foi enviado com sucesso.</p>
+                <p>Obrigado...</p>
+            `
+            $('#cForm').html(feedback)
+        }
+    })
 
     return false
 }
